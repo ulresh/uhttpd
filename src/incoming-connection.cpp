8fc7a020dd3c868f7286d86aefdd0adafcac5c5a
Yury Reshetnikov <reshu@mail.ru>
2024-03-10 22:20:48 +0500

    _

--- src/incoming-connection.cpp
+++ src/incoming-connection.cpp
@@ -6,7 +6,89 @@ IncomingConnection::IncomingConnection(Server &server)
 {
 }
 
+void IncomingConnection::close() {
+	// TODO остановить таймер чтения, удалиться из списков
+}
+
 void IncomingConnection::async_read() {
+	buffer.reset(new char[header_buffer_size()]);
+	state = 0; header_size = 0;
+	async_read_header();
+}
+
+void IncomingConnection::async_read_header() {
+	socket.async_read_some(ai::buffer(buffer.get(), header_buffer_size()),
+			boost::bind(&IncomingConnection::handle_read_header,
+			this, shared_from_this(), ph::error, ph::bytes_transferred));
+}
+
+/* method /uri+uri%aburi?name=value&name2=value#anchor proto
+  0 1    2 3      4     5
+   header: value
+     continued value
+
+   data
+ */
+
+void IncomingConnection::handle_read_header(
+		std::shared_ptr<IncomingConnection>,
+		const error_code& error, std::size_t bytes_transferred) {
+	if(closing || server.closing) return;
+	else if(error) { close(); return; }
+	if(!bytes_transferred) goto read_next_chunk;
+	char *ptr = beffer.get(), *mark = ptr, *end = ptr + bytes_transferred,
+		*end1 = end - 1;
+	for(; ptr < end; ++ptr)
+		switch(state) {
+		default: close(); return;
+		case 0:
+			if(*ptr < 'A' || *ptr > 'Z') { close(); return; }
+			++state;
+			if(ptr < end1) ++ptr; else {
+				method.append(mark, end - mark);
+				goto read_next_chunk;
+			}
+		case 1:
+			while(*ptr >= 'A' && *ptr <= 'Z') {
+				if(++ptr == end) {
+					method.append(mark, end - mark);
+					goto read_next_chunk;
+				}
+			}
+			if(*ptr == ' ') {
+				++state;
+				if(ptr < end1) ++ptr; else goto read_next_chunk;
+			}
+			else { close(); return; }
+		case 2:
+			while(*ptr == ' ') if(++ptr == end) goto read_next_chunk;
+			mark = ptr;
+			++state;
+		case 3:
+			switch(*ptr) {
+			case ' ':
+			case '?':
+			case '#':
+			case '\r':
+			case '\n':
+				if(ptr == mark) {
+					if(url.empty()) { close(); return; }
+				}
+				else url.append(mark, ptr - mark);
+			// EDIT POINT
+			case '+':
+			case '%':
+			default:
+				;
+			}
+		}
+	switch(state) {
+	default: close(); return;
+	}
+ read_next_chunk:
+	header_size += bytes_transferred;
+	if(header_size >= max_request_header_size()) { close(); return; }
+	async_read_header();
 }
 
 /*
--- src/incoming-connection.hpp
+++ src/incoming-connection.hpp
@@ -6,11 +6,28 @@ struct Server;
 
 struct IncomingConnection :
 	std::enable_shared_from_this<IncomingConnection> {
+
+	struct Variable {
+		std::string name, value;
+	};
+
 	IncomingConnection(Server &server);
+	void close();
 	void async_read();
+	void async_read_header();
+	static int header_buffer_size() { return 4096; }
+	static int max_request_header_size() { return 128 * 1024; }
+	static int max_request_body_size() { return 128 * 1024; }
+	void handle_read_header(std::shared_ptr<IncomingConnection>,
+							const error_code& error,
+							std::size_t bytes_transferred);
 	Server &server;
 	tcp::socket socket;
 	bool closing = false;
+	std::unique_ptr<char[]> buffer;
+	int state, up_state, header_size;
+	std::string method, url, proto, connection;
+	std::list<Variable> query, headers, post;
 };
 
 /*
