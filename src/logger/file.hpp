2aa355a50734cbc6704b7a628f79f60f0be90228
Yury Reshetnikov <reshu@mail.ru>
2024-03-23 18:32:23 +0500

    _

--- src/logger/file.hpp
+++ src/logger/file.hpp
@@ -6,13 +6,16 @@
 namespace Logger {
 struct File {
 	File() {}
+	void reset_stream(const std::string &filepath);
 	void open(io_context &ios, const std::string &filepath);
+	void reopen(const std::string &filepath);
 	void post(TextShp text);
 	void write(TextShp text);
 	void ready(TextShp text, const error_code& error);
 	io_context *iosp = nullptr;
 	std::unique_ptr<ai::stream_file> stream;
 	bool busy = true, disabled = false;
+	std::string reopen_filepath;
 	ptime skipped_start;
 	long skipped_events = 0, skipped_size = 0;
 	std::array<char, 1024*1024> buffers[2];
