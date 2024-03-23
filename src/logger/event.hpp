430fa86e0877b651304bb04852536e2bca503758
Yury Reshetnikov <reshu@mail.ru>
2024-03-23 07:04:11 +0500

    _

--- src/logger/event.hpp
+++ src/logger/event.hpp
@@ -7,10 +7,20 @@
 namespace Logger {
 struct File;
 struct Event : std::ostream {
-	explicit Event(File &file);
+	Event(File &file);
 	~Event();
 	operator bool () const;
 	// void send() { flush(); Event sender(file); std::swap(*this, sender); }
+	File &file;
+	const TextShp text;
+	Streambuf buffer;
+};
+
+struct DirectEvent : std::ostream {
+	DirectEvent(File &file);
+	~DirectEvent();
+	static int max_size() { return 256; }
+	operator bool () const;
 	File &file;
 	const TextShp text;
 	Streambuf buffer;
